f24b1d59a48cab6cf0b133f5a0c81be5dd32c889
Michael James <michael.james@mksoftwaresolutions.co.uk>
2012-11-07 23:13:14 +0000

    created nuget packages for the libraries

--- CDLQIntegration/Properties/AssemblyInfo.cs
+++ CDLQIntegration/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CDLQIntegration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MKS.CDLQIntegration")]
+[assembly: AssemblyDescription("C# Wrapper for the CDLQ VRM Service")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MK Software Solutions")]
 [assembly: AssemblyProduct("CDLQIntegration")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
